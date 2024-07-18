d9f6cbd339588d6cebd5bf3d210af7b03b520763
jjehff <152602266+jjehff@users.noreply.github.com>
2024-07-18 12:12:54 -0700

    Update script.js

--- script.js
+++ script.js
@@ -10,19 +10,40 @@ const resume = document.getElementById("resume");
 const legal1 = document.getElementById("legal1");
 const legal2 = document.getElementById("legal2");
 //use this button to add work might need to be hidden in the begining
-const addWorkBtn = document.getElementById("addWork"); //<- add these in html
-const doneAddingBtn = document.getElementById("doneAdding");// <- add these in html JEFF
-const endDialog;  //add necessary element in HTML
-const acceptCloseBtn = document.getElementById("acceptClose");// add this in html
-const cancelCloseBtn = document.getElementById("cancelClose");//,- add this in html
+const addWorkBtn = document.getElementById("add-past-work"); //<- add these in html ------------|
+const doneAddingBtn = document.getElementById("done-adding");// <- add these in html JEFF ------|
+// const endDialog;  //add necessary element in HTML                                            |---- all done
+const acceptCloseBtn = document.getElementById("accept-close");// add this in html -------------|
+const cancelCloseBtn = document.getElementById("cancel-close");//,- add this in html -----------|
 const submitBtn = document.getElementById("save");
 
 //addWorkBtn.addEventListener("click", () => taskForm.classList.toggle("hidden"));
 //use this if i need to hide shit. this will defniteley change
 
-doneAddingBtn.addEventListener("click", ()=> endDialog.showModal());
+addWorkBtn.addEventListener("click", () => {
+    const container = document.getElementById("work-experience-box").style.display = "block";
+})
 
+doneAddingBtn.addEventListener("click", ()=> {
+    const discardBox = document.getElementsByClassName("discard");
+    for (let i = 0; i < discardBox.length; i++) {
+        discardBox[i].style.display = "block";
+    }
+    acceptCloseBtn.addEventListener("click", () => removeAll());
+    cancelCloseBtn.addEventListener("click", ()=>{
+        for (let i = 0; i < discardBox.length; i++) {
+            discardBox[i].style.display = "none";
+        }
+    });
+});
 
+const removeAll = () => {
+    const discardBox = document.getElementsByClassName("discard");
+    const container = document.getElementById("work-experience-box").style.display = "none";
+    for (let i = 0; i < discardBox.length; i++) {
+        discardBox[i].style.display = "none";
+    }
+}
 
 
 
