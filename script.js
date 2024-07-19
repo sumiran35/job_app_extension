ae26ef279f743328be4cf1007479c2a2e94188f6
jjehff <152602266+jjehff@users.noreply.github.com>
2024-07-18 19:23:25 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 const fname = document.getElementById("fname");
-const lname = document.getElementById("lname");
+const lmane = document.getElementById("lname");
 const address = document.getElementById("address");
 const city = document.getElementById("city");
 const state = document.getElementById("state");
@@ -12,18 +12,23 @@ const legal2 = document.getElementById("legal2");
 //use this button to add work might need to be hidden in the begining
 const addWorkBtn = document.getElementById("add-past-work"); //<- add these in html ------------|
 const doneAddingBtn = document.getElementById("done-adding");// <- add these in html JEFF ------|
-// const endDialog;  //add necessary element in HTML
-// |---- all done
-const company  = document.getElementById("company");
-const position = document.getElementById("position");
-const workStartDate = document.getElementById("work_start_date");
-const workEndDate = document.getElementById("work_end_date");
-const reponsibility = document.getElementById("reponsibility");
-
-
+// const endDialog;  //add necessary element in HTML                                            |---- all done
 const acceptCloseBtn = document.getElementById("accept-close");// add this in html -------------|
 const cancelCloseBtn = document.getElementById("cancel-close");//,- add this in html -----------|
 const submitBtn = document.getElementById("save");
+const submitWorkBtn = document.getElementById("save-work-experience");
+const listPastJobs = document.getElementById("inputted-job-experience");
+
+//addWorkBtn.addEventListener("click", () => taskForm.classList.toggle("hidden"));
+//use this if i need to hide shit. this will defniteley change
+
+
+const container = document.getElementById("work-experience-box");
+const company  = document.getElementById("company");
+const position = document.getElementById("position");
+const workStartDate = document.getElementById("work-start-date");
+const workEndDate = document.getElementById("work-end-date");
+const reponsibility = document.getElementById("reponsibility");
 
 const addEducationBtn = document.getElementById("add-education");
 const levelOfEducation = document.getElementById("level-of-education");
@@ -33,14 +38,13 @@ const schoolStartDate = document.getElementById("school-start-date");
 const schoolEndDate = document.getElementById("school-end-date");
 const doneAddingEducationBtn = document.getElementById("done-add-education");
 
-//addWorkBtn.addEventListener("click", () => taskForm.classList.toggle("hidden"));
-//use this if i need to hide shit. this will defniteley change
+addWorkBtn.addEventListener("click", () => {showPastWorkBox()});     // Runs when Add Past Work Experience is pressed
 
-addWorkBtn.addEventListener("click", () => {
-    const container = document.getElementById("work-experience-box").style.display = "block";
-})
+const showPastWorkBox = () => {
+    container.style.display = "block";
+};
 
-doneAddingBtn.addEventListener("click", ()=> {
+doneAddingBtn.addEventListener("click", ()=> {                  // Runs when X is pressed
     const discardBox = document.getElementsByClassName("discard");
     for (let i = 0; i < discardBox.length; i++) {
         discardBox[i].style.display = "block";
@@ -53,56 +57,90 @@ doneAddingBtn.addEventListener("click", ()=> {
     });
 });
 
-const removeAll = () => {
+submitWorkBtn.addEventListener("click", () => {              // Runs when submit button for PAST JOBS is pressed, pushes info to array
+    const pastJob = {                                       // and updates ui
+        company: company.value,
+        position: position.value,
+        startDate: workStartDate.value,
+        endDate: workEndDate.value,
+        responsibility: responsibility.value
+    };
+    allJobAppStorage.push(pastJob);
+
+    listPastJobs.innerHTML += `<div id="temp"><br />
+    Company: ${company.value} <br />
+    Position: ${position.value} <br />
+    Time: ${workStartDate.value} -> ${workEndDate.value} <br />
+    Description: ${responsibility.value} <br />
+    <button onclick="editPastWork()" type="button">Edit</button>
+    <button onclick="removePastWork()" type="button">Delete</button>
+    <br /></div>
+    `
+    alert("Successfully saved!");
+    removeAll();
+});
+
+const clearJobInfo = () => {                            // Clears information in past work info boxes
+    company.value = "";
+    position.value = "";
+    workStartDate.value = "";
+    workEndDate.value = "";
+    responsibility.value = "";
+};
+
+const removeAll = () => {                                           // Helper function to remove past work boxes
+    clearJobInfo();
     const discardBox = document.getElementsByClassName("discard");
     const container = document.getElementById("work-experience-box").style.display = "none";
     for (let i = 0; i < discardBox.length; i++) {
         discardBox[i].style.display = "none";
     }
-}
+};
 
+const removePastWork = () => {
+    const removeDiv = document.getElementById("temp");
+    removeDiv.innerHTML = ""; 
+    alert(allJobAppStorage[0][company.value]);
+};
 
-
+const editPastWork = () => {
+    alert("hei");
+    const editDiv = document.getElementById("temp");
+    showPastWorkBox();
+};
 
 const allJobAppStorage = []; //array that stores objects
-let currJobApp = {
-    uniqueId: "", //need to create a hash or something here to assign unique values based on unique strings to see if
-    //a app already exists
-    f_name: fname.value.toLowerCase(),
-    l_name: lname.value.toLowerCase(),
-    email: email.value.toLowerCase(),
-    address: address.value.toLowerCase(),
-    city: city.value.toLowerCase(),
-    state: state.value.toLowerCase(),
+let currentJobApp = {
+    f_name: fname.value,
+    l_name: lname.value,
+    email: email.value,
+    address: address.value,
+    city: city.value,
+    state: state.value,
     postal: zip.value,
     phone: phone.value,
     //resume : as BOO potentially?
-    workExp: {company: company.value.toLowerCase().split(" ").join("-"),
-        position: position.value.toLowerCase().split(" ").join("-"),
-        startDate: workStartDate.value,//might need the same method here as on top
-        endDate: workEndDate.value,//might need the same shit here as on top
-        responsibility: reponsibility.value.toLowerCase().split(" ").join("-")},
+    workExp: {
+        company: company.value,
+        position: position.value,
+        startDate: workStartDate.value,
+        endDate: workEndDate.value,
+        responsibility: reponsibility.value
+    },
     education: {
-        educationLevel: levelOfEducation.value.toLowerCase().split(" ").join("-"),
-        school: schoolName.value.toLowerCase().split(" ").join("-"),
-        degree: degreeName.value.toLowerCase().split(" ").join("-"),
-        start: schoolStartDate.value,//might need the same shit here as on top
-        end: schoolEndDate.value//might need the same shit here as on top
-    } //NOTE TO JEFF:- use the mthod applied stuff to generate unique ids
-
-
+        educationLevel: levelOfEducation.value,
+        school: schoolName.value,
+        degree: degreeName.value,
+        start: schoolStartDate.value,
+        end: schoolEndDate.value
+    }
 
 
 };  //object to store current values.
 
 //need a function to store the object into the task form
-//NEED SOMETHING to check for if element already exists
-//here using position ad unique id but we need to chane this for sure
-const alreadyExistsApp = allJobAppStorage.indexOf((curr) => curr.uniqueId === currJobApp.uniqueId );
-const addToList = (jobApp = {}) => {};
- if(alreadyExistsApp === -1){
-     allJobAppStorage.unshift(currJobApp);
- }
+
+const addTolist = (jobapp = {}) => {};
 
 acceptCloseBtn.addEventListener("click", () => endDialog.close());//primarily used to ask for confirmation
 //before ending stuff.
